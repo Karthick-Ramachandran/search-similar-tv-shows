9601971011b7037cb736ff01aae27c988d8f0306
karthick <karthiram165@gmail.com>
2019-05-21 16:56:35 +0530

    api

---
+++ precache-manifest.1e0ba69f6341b32c34267081d58a6283.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f3dbe50b17c2f6725f2f240600afc63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd37c31645526a5f92e3",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "561318b94763c2757798",
+    "url": "/static/js/2.241e526e.chunk.js"
+  },
+  {
+    "revision": "fd37c31645526a5f92e3",
+    "url": "/static/js/main.1dc7c6e0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
